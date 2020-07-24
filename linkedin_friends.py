44e67d768336f456d2e7b49f09a936fa1b634e79
Slava <urigami2010@gmail.com>
2020-07-24 14:25:57 +0300

    Init

---
+++ linkedin_friends.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from settings import CHROME_PATH, username, password, MOBILE_UA, amount_of_people_to_add
+import pyautogui
+import time
+
+
+def main():
+    options = Options()
+    options.add_argument(f"user-agent={MOBILE_UA}")
+    driver = webdriver.Chrome(CHROME_PATH, chrome_options=options)
+    driver.get('https://www.linkedin.com/login/ru?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+
+    try:
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+        driver.find_element_by_xpath('//*[@id="app__container"]/main/div[2]/div[2]/form/div[3]/button').click()
+    except:
+        pass
+
+    try:
+        driver.get('https://www.linkedin.com/mynetwork/')
+        time.sleep(2)
+        for i in range(amount_of_people_to_add):
+            btn_location = pyautogui.locateOnScreen('btn_img.PNG')
+            pyautogui.click(btn_location)
+            print(f'Sent a connection request => #{i}')
+            pyautogui.keyDown('down')
+            pyautogui.keyDown('down')
+            pyautogui.keyDown('down')
+
+    except:
+        pass
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,8 @@
+CHROME_PATH = 'chromedriver.exe'
+MOBILE_UA = 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F)' \
+            ' AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+
+# Credentials:
+username = 'username@gmail.com'
+password = 'password'
+amount_of_people_to_add = 50
