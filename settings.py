4637972e1aa97c3fb89bb91db471a09c97ac2f33
Slava <urigami2010@gmail.com>
2020-07-28 09:13:25 +0300

    Tweaking

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ MOBILE_UA = 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL
             ' AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
 
 # Credentials:
-username = 'urigami2010@gmail.com'
-password = '91739519173951Dd'
+username = 'user@gmail.com'
+password = 'psw'
 amount_of_people_to_add = 79
